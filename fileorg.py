7c809e108b50277e0ab00da5421d24478f1fbc9f
Fatih Yağcı <117002802+FatihYagci@users.noreply.github.com>
2023-10-26 08:27:28 +0300

    Update fileorg.py

--- fileorg.py
+++ fileorg.py
@@ -7,26 +7,25 @@ import os
 direcory = "C:/Users/-name-/Downloads"
 
 categories = {
-    'Images': ['.jpeg', '.jpg', '.tiff', '.gif', '.bmp', '.png', '.bpg', 'svg'],
-    'Videos': ['.webm', '.avi', '.flv', '.wmv', '.mov', '.mp4', '.webm', '.vob', '.mng', '.qt', '.mpg', '.mpeg', '.3gp'],
-    'Pdfs': ['.pdf'],
-    'Documents': ['.txt', '.oxps', '.epub', '.pages', '.docx', '.doc', '.fdf', '.ods', '.odt', '.pwi', '.xsn', '.xps', '.dotx', '.docm', '.dox', '.rvg', '.rtf', '.rtfd', '.wpd', '.xls', '.xlsx', '.ppt', 'pptx'],
+    '1-Images': ['.jpeg', '.jpg', '.tiff', '.gif', '.bmp', '.png', '.bpg', 'svg'],
+    '2-Videos': ['.webm', '.avi', '.flv', '.wmv', '.mov', '.mp4', '.webm', '.vob', '.mng', '.qt', '.mpg', '.mpeg', '.3gp'],
+    '3-Documents': ['.txt', '.oxps', '.epub', '.pages', '.docx', '.doc', '.fdf', '.ods', '.odt', '.pwi', '.xsn', '.xps', '.dotx', '.docm', '.dox', '.rvg', '.rtf', '.rtfd', '.wpd', '.xls', '.xlsx', '.ppt', 'pptx'],
+    '4-Zips': ['.zip', '.rar', '.tar', '.iso', '.7z', '.gz'],
+    '5-Pdfs': ['.pdf']
 }
-for category in ['Images', 'Videos', 'Pdfs', 'Documents']:
+    
+for category in ['1-Images', '2-Videos', '3-Documents', '4-Zips', '5-Pdfs']:
     os.makedirs(os.path.join(direcory, category), exist_ok=True)
-
-#this was supposed to be an easy task man why it has bugs???
-#i think the bug i have rn is in my def function will check soon
 
 #function to classify files
 def classify_file(filename):
     #finding extension
-    extension = filename.split('.')[-1]
+    extension = '.' + filename.split('.')[-1]
 
     #choosing a category
     for category, extensions in categories.items():
         #checking if the extension is in the category to move file afterwards
-        if extension in extension:
+        if extension in extensions:
             #constructing file path
             source_path = os.path.join(direcory, filename)
             dest_path = os.path.join(direcory, category, filename)
@@ -34,6 +33,7 @@ def classify_file(filename):
             os.rename(source_path, dest_path)
             print(f"Moved {filename} to {category}")
             break
+
 
 #clasiifying current files in directory
 for filename in os.listdir(direcory):
@@ -46,7 +46,7 @@ initial_files = os.listdir(direcory)
 #does all the shinanigans
 while True:
 
-    time.slee(5)
+    time.sleep(5)
     current_files = os.listdir(direcory)
 
     new_files = os.listdir(set(current_files) - set(initial_files))
